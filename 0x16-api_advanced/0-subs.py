4021f3e59452cc43878ed156a4fc775130a873e9
Michael Megbenu <msk.megbenu@gmail.com>
2024-05-08 00:31:31 +0200

    adv-api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" How many subs? """
+import requests
+from requests import get
+from sys import argv
+
+
+def number_of_subscribers(subreddit):
+    # Replace these values with your Reddit application details
+    client_id = 'E9UcaSW-udVFniBFD8Cf6w'
+    client_secret = 'tiSdtSn31T0MlzpPz3ZGyme7VhAuSg'
+    head = {'User-Agent': 'michael'}
+    # Reddit API endpoint for subreddit information
+    url = 'https://oauth.reddit.com/r/{}/about.json'.format(subreddit)
+
+    # Set up the headers with authentication
+    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+    header = head
+
+    # Make a GET request to the API
+    response = requests.get(url, headers=header, auth=auth,
+                            allow_redirects=False)
+
+    # Check if the request was successful (status code 200) and not redirected
+    if response.status_code == 200 and not response.is_redirect:
+        # Parse the JSON response and extract the number of subscribers
+        data = response.json()
+        subscribers_count = data['data']['subscribers']
+        return subscribers_count
+    else:
+        # Return 0 for invalid subreddit or other errors
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+# 0x16-api_advanced
+Michael Megbenu @devmike93
+www.mikedevx.tech
